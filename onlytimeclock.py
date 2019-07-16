306bffa29bbf1e148350d90f25f157f70df486fc
lordyka <lordyka@gmail.com>
2019-07-16 18:25:05 +0200

    change the size and positions of the numbers

--- onlytimeclock.py
+++ onlytimeclock.py
@@ -13,33 +13,35 @@ unicorn.brightness(0.5)
 
 
 # There are 4 different types of patterns used when generating
-# a number that is to be placed in a rectangle 3X5 pixels. Combinations of these
+# a number that is to be placed in a rectangle 7x7 pixels. Combinations of these
 # are used to create a number pattern such as:
-#   * * *
+#   * * * * * * *
 #   *
-#   * * *
-#   *   *
-#   * * *
+#   *
+#   * * * * * * *
+#   *           *
+#   *           *
+#   * * * * * * *
 
-# 1) * * * Full Row
-# 2) *   * Both Sides
-# 3)     * Right Side
-# 4) *     Left Side
+# 1) * * * * * * * Full Row
+# 2) *           * Both Sides
+# 3)             * Right Side
+# 4) *             Left Side
 
 # Composition methods
 def fullLine(start, row):
-  for x in range(start, start+3):
+  for x in range(start, start+8):
     unicorn.set_pixel(x, row, 255, 255, 255)
 
 def bothSides(start, row):
   unicorn.set_pixel(start, row, 255, 255, 255)
-  unicorn.set_pixel(start+2, row, 255, 255, 255)
+  unicorn.set_pixel(start+7, row, 255, 255, 255)
 
 def leftSide(start, row):
   unicorn.set_pixel(start, row, 255, 255, 255)
 
 def rightSide(start, row):
-  unicorn.set_pixel(start+2, row, 255, 255, 255)
+  unicorn.set_pixel(start+7, row, 255, 255, 255)
 
 # Numbers
 def displayZero(x, y):
@@ -48,7 +50,9 @@ def displayZero(x, y):
   bothSides(x, y-1)
   bothSides(x, y-2)
   bothSides(x, y-3)
-  fullLine(x, y-4)
+  bothSides(x, y-4)
+  bothSides(x, y-5)
+  fullLine(x, y-6)
   unicorn.show()
 
 def displayOne(x, y):
@@ -58,51 +62,63 @@ def displayOne(x, y):
   rightSide(x, y-2)
   rightSide(x, y-3)
   rightSide(x, y-4)
+  rightSide(x, y-5)
+  rightSide(x, y-6)
   unicorn.show()
 
 def displayTwo(x, y):
   clearNumberPixels(x, y)
   fullLine(x, y)
   rightSide(x, y-1)
-  fullLine(x, y-2)
-  leftSide(x, y-3)
-  fullLine(x, y-4)
+  rightSide(x, y-2)
+  fullLine(x, y-3)
+  leftSide(x, y-4)
+  leftSide(x, y-5)
+  fullLine(x, y-6)
   unicorn.show()
 
 def displayThree(x, y):
   clearNumberPixels(x, y)
   fullLine(x, y)
   rightSide(x, y-1)
-  fullLine(x, y-2)
-  rightSide(x, y-3)
-  fullLine(x, y-4)
+  rightSide(x, y-2)
+  fullLine(x, y-3)
+  rightSide(x, y-4)
+  rightSide(x, y-5)
+  fullLine(x, y-6)
   unicorn.show()
 
 def displayFour(x, y):
   clearNumberPixels(x, y)
   bothSides(x, y)
   bothSides(x, y-1)
-  fullLine(x, y-2)
-  rightSide(x, y-3)
+  bothSides(x, y-2)
+  fullLine(x, y-3)
   rightSide(x, y-4)
+  rightSide(x, y-5)
+  rightSide(x, y-6)
   unicorn.show()
 
 def displayFive(x, y):
   clearNumberPixels(x, y)
   fullLine(x, y)
   leftSide(x, y-1)
-  fullLine(x, y-2)
-  rightSide(x, y-3)
-  fullLine(x, y-4)
+  leftSide(x, y-2)
+  fullLine(x, y-3)
+  rightSide(x, y-4)
+  rightSide(x, y-5)
+  fullLine(x, y-6)
   unicorn.show()
 
 def displaySix(x, y):
   clearNumberPixels(x, y)
   fullLine(x, y)
   leftSide(x, y-1)
-  fullLine(x, y-2)
-  bothSides(x, y-3)
-  fullLine(x, y-4)
+  leftSide(x, y-2)
+  fullLine(x, y-3)
+  bothSides(x, y-4)
+  bothSides(x, y-5)
+  fullLine(x, y-6)
   unicorn.show()
 
 def displaySeven(x, y):
@@ -112,24 +128,30 @@ def displaySeven(x, y):
   rightSide(x, y-2)
   rightSide(x, y-3)
   rightSide(x, y-4)
+  rightSide(x, y-5)
+  rightSide(x, y-6)
   unicorn.show()
 
 def displayEight(x, y):
   clearNumberPixels(x, y)
   fullLine(x, y)
   bothSides(x, y-1)
-  fullLine(x, y-2)
-  bothSides(x, y-3)
-  fullLine(x, y-4)
+  bothSides(x, y-2)
+  fullLine(x, y-3)
+  bothSides(x, y-4)
+  bothSides(x, y-5)
+  fullLine(x, y-6)
   unicorn.show()
 
 def displayNine(x, y):
   clearNumberPixels(x, y)
   fullLine(x, y)
   bothSides(x, y-1)
-  fullLine(x, y-2)
-  rightSide(x, y-3)
-  fullLine(x, y-4)
+  bothSides(x, y-2)
+  fullLine(x, y-3)
+  rightSide(x, y-4)
+  rightSide(x, y-5)
+  fullLine(x, y-6)
   unicorn.show()
 
 def displayNumber(x,y, number):
@@ -155,10 +177,10 @@ def displayNumber(x,y, number):
     displayNine(x,y)
 
 # Clears the pixels in a rectangle. x,y is the top left corner of the rectangle
-# and its dimensions are 3X5
+# and its dimensions are 7X7
 def clearNumberPixels(x, y):
-  for y1 in range(y, y-5, -1):
-    for x1 in range(x, x+3):
+  for y1 in range(y, y-7, -1):
+    for x1 in range(x, x+7):
       # print("x1 = "+str(x1)+" y1 = "+str(y1))
       unicorn.set_pixel(x1, y1, 0, 0, 0)
   unicorn.show()
@@ -181,9 +203,9 @@ displayedMinuteParts = getTimeParts('%M')
 
 # Display Current Time
 displayNumber(0,15, displayedHourParts[0])
-displayNumber(4,15, displayedHourParts[1])
-displayNumber(9,15, displayedMinuteParts[0])
-displayNumber(13,15, displayedMinuteParts[1])
+displayNumber(8,15, displayedHourParts[1])
+displayNumber(0,6, displayedMinuteParts[0])
+displayNumber(8,6, displayedMinuteParts[1])
 
 current_milli_time = lambda: int(round(time.time() * 1000))
 
@@ -203,17 +225,17 @@ try:
     # Only update second hour number if it is different to what is currently displayed
     if hourParts[1] != displayedHourParts[1]:
       displayedHourParts[1] = hourParts[1]
-      displayNumber(4,15, hourParts[1])
+      displayNumber(8,15, hourParts[1])
 
     # Only update first minute number if it is different to what is currently displayed
     if minuteParts[0] != displayedMinuteParts[0]:
       displayedMinuteParts[0] = minuteParts[0]
-      displayNumber(9,15, minuteParts[0])
+      displayNumber(0,6, minuteParts[0])
 
     # Only update second minute number if it is different to what is currently displayed
     if minuteParts[1] != displayedMinuteParts[1]:
       displayedMinuteParts[1] = minuteParts[1]
-      displayNumber(13,15, minuteParts[1])
+      displayNumber(8,6, minuteParts[1])
 
 except KeyboardInterrupt:
   unicorn.off()
