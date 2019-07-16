4ec4e57bb40e586981caf736890d6a2cdf6e9cd0
lordyka <lordyka@gmail.com>
2019-07-16 16:44:29 +0200

    remove date and dots #1
    remove unnecessary part of the code

--- onlytimeclock.py
+++ onlytimeclock.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     from unicorn_hat_sim import unicornhathd as unicorn
 
-unicorn.rotation(180)
+unicorn.rotation(270)
 unicorn.brightness(0.5)
 
 secondsProgressRow = 10
@@ -165,19 +165,7 @@ def clearNumberPixels(x, y):
       unicorn.set_pixel(x1, y1, 0, 0, 0)
   unicorn.show()
 
-def displayTimeDots(x, y):
-  unicorn.set_pixel(x, y-1, 255, 0, 0)
-  unicorn.set_pixel(x, y-3, 255, 0, 0)
-  unicorn.show()
 
-def displayDateDots(x, y):
-  unicorn.set_pixel(x+1, y, 255, 0, 0)
-  unicorn.set_pixel(x+1, y-1, 255, 0, 0)
-  unicorn.set_pixel(x+1, y-2, 255, 0, 0)
-  unicorn.set_pixel(x, y-2, 255, 0, 0)
-  unicorn.set_pixel(x, y-3, 255, 0, 0)
-  unicorn.set_pixel(x, y-4, 255, 0, 0)
-  unicorn.show()  
 
 # Gets a specific part of the current time, passed to strftime, then it is
 # split into its individual numbers and converted into integers. Used to feed
@@ -190,40 +178,14 @@ def getTimeParts(timePart):
   parts = datetime.datetime.now().strftime(timePart)
   return [int(x) for x in parts]  
 
-def setStartingSecondsProgressRow():
-  # Grey starting bit
-  unicorn.set_pixel(0, secondsProgressRow, 100, 100, 100)
-  unicorn.set_pixel(1, secondsProgressRow, 100, 100, 100)
-
-  # Making red part
-  for x in range(2,14):
-    unicorn.set_pixel(x, secondsProgressRow, 200, 50, 50)
-
-  # Grey ending bit
-  unicorn.set_pixel(14, secondsProgressRow, 100, 100, 100)
-  unicorn.set_pixel(15, secondsProgressRow, 100, 100, 100)
-
 displayedHourParts = getTimeParts('%H')
 displayedMinuteParts = getTimeParts('%M')
-displayedMonthParts = getTimeParts('%m')
-displayedDayParts = getTimeParts('%d')
 
 # Display Current Time
 displayNumber(0,15, displayedHourParts[0])
 displayNumber(4,15, displayedHourParts[1])
-displayTimeDots(7,15)
 displayNumber(9,15, displayedMinuteParts[0])
 displayNumber(13,15, displayedMinuteParts[1])
-
-# Display Day and Month
-displayNumber(0,9, displayedDayParts[0])
-displayNumber(4,9, displayedDayParts[1])
-displayDateDots(7,9)
-displayNumber(9,9, displayedMonthParts[0])
-displayNumber(13,9, displayedMonthParts[1])
-
-# set the seconds progress row
-setStartingSecondsProgressRow()
 
 current_milli_time = lambda: int(round(time.time() * 1000))
 
@@ -233,9 +195,7 @@ try:
   while True:
     hourParts = getTimeParts('%H')
     minuteParts = getTimeParts('%M')
-    dayParts = getTimeParts('%d')
-    monthParts = getTimeParts('%m')
-
+    
     # TIME Details
     # Only update first hour number if it is different to what is currently displayed
     if hourParts[0] != displayedHourParts[0]:
@@ -257,44 +217,5 @@ try:
       displayedMinuteParts[1] = minuteParts[1]
       displayNumber(13,15, minuteParts[1])
 
-    # MONTH Details
-    # Only update first day number if it is different to what is currently displayed
-    if dayParts[0] != displayedDayParts[0]:
-      displayedDayParts[0] = dayParts[0]
-      displayNumber(0,9, dayParts[0])
-
-    # Only update second day number if it is different to what is currently displayed
-    if dayParts[1] != displayedDayParts[1]:
-      displayedDayParts[1] = dayParts[1]
-      displayNumber(4,9, dayParts[1])
-
-    # Only update first month number if it is different to what is currently displayed
-    if monthParts[0] != displayedMonthParts[0]:
-      displayedMonthParts[0] = monthParts[0]
-      displayNumber(9,9, monthParts[0])
-
-    # Only update second month number if it is different to what is currently displayed
-    if monthParts[1] != displayedMonthParts[1]:
-      displayedMonthParts[1] = monthParts[1]
-      displayNumber(13,9, monthParts[1])
-
-    # Calculating how many seconds pixels to light up
-    currentSeconds = datetime.datetime.now().second
-    percentOfMinuteComplete = int((float(currentSeconds)/60.0)*100)
-    pixelsToLightUp = int((12.0/100.0)*float(percentOfMinuteComplete))
-
-    # Make the whole progress row red
-    if pixelsToLightUp == 0:
-      setStartingSecondsProgressRow()
-
-    # Making completed pixels green
-    for x in range(2, pixelsToLightUp+2):
-      unicorn.set_pixel(x, secondsProgressRow, 0, 100, 0)
-
-    # Setting current pixel brighter green
-    unicorn.set_pixel(pixelsToLightUp+2, secondsProgressRow, 0, 255, 0)
-    unicorn.show()
-    # Sleep for 0.5 because the display doesn't need to update that often
-    time.sleep(0.5)
 except KeyboardInterrupt:
   unicorn.off()
